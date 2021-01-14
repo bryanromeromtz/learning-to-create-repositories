93c2739a323b2bb40db830f3099cdc477cf1554e
bryanromeromtz <cryptomtz@hotmail.com>
2021-01-14 17:14:03 -0600

    Aded new python function

--- my_file.py
+++ my_file.py
@@ -1,7 +1,20 @@
 print('Hi Git')
+print('Hi Git')
 
 
 print('Hi from another dev')
 
-def hi_there() :
+
+def hi_there():
   print('Hi there')
+  
+def hi_there():
+  print('Hi there')  
+
+
+def mi_new_function():
+    return ('HI')
+
+print("Hi again")
+  
+
